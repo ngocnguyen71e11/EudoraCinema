e7a2f97332f73a6f9a65d1ebdf791c31f5ce589d
Hong Ngoc <ngocnguyen71e11@gmail.com>
2023-03-30 17:55:09 +0700

    commit add class

---
+++ EudoraCinema/Models/PhimEnity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EudoraCinema.Models
+{
+    public class PhimEnity
+    {
+
+        private int m_PK_iPhimID;
+        private string m_sTenphim;
+        private string m_sThoiluong;
+        private DateTime m_dNgaykhoichieu;
+        private string m_sDaodien;
+        private string m_sDienvien;
+        private string m_sMota;
+        private string m_sNgonngu;
+        private string m_sAnh;
+
+        public int PK_iPhimID { get => m_PK_iPhimID; set => m_PK_iPhimID = value; }
+        public string sTenphim { get => m_sTenphim; set => m_sTenphim = value; }
+        public string sThoiluong { get => m_sThoiluong; set => m_sThoiluong = value; }
+        public DateTime dNgaykhoichieu { get => m_dNgaykhoichieu; set => m_dNgaykhoichieu = value; }
+        public string sDaodien { get => m_sDaodien; set => m_sDaodien = value; }
+        public string sDienvien { get => m_sDienvien; set => m_sDienvien = value; }
+        public string sMota { get => m_sMota; set => m_sMota = value; }
+        public string sNgonngu { get => m_sNgonngu; set => m_sNgonngu = value; }
+        public string sAnh { get => m_sAnh; set => m_sAnh = value; }
+    }
+}
