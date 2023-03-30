7fd1b63ebb42aef9cc6aba2b8f8a0e9be0ab5237
Hong Ngoc <ngocnguyen71e11@gmail.com>
2023-03-30 17:10:17 +0700

    Add project files.

---
+++ EudoraCinema/Controllers/TaikhoanController.cs
@@ -0,0 +1,39 @@
+﻿using EudoraCinema.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EudoraCinema.Controllers
+{
+    public class TaikhoanController : Controller
+    {
+        // GET: Taikhoan
+        public ActionResult Login(FormCollection collection)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string Email = collection["sEmail"];
+                string Pass = collection["sMatkhau"];
+                //httpClient.BaseAddress = new Uri(UriString);
+                using (HttpResponseMessage response = httpClient.GetAsync("http://192.168.1.26:8043/api/TaikhoanAPI/"+Email+"/"+Pass).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return View("Index");
+                    }
+                    else
+                    {
+                        // Handle error response
+                        return View();
+                    }
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ EudoraCinema/Models/TaikhoanEntity.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace EudoraCinema.Models
+{
+    public class TaikhoanEntity
+    {
+        private int m_PK_iTaikhoanID;
+        private string m_sPhanquyen;
+        private string m_sMatkhau;
+        private string m_sEmail;
+
+        public int PK_iTaikhoanID { get => m_PK_iTaikhoanID; set => m_PK_iTaikhoanID = value; }
+        public string sPhanquyen { get => m_sPhanquyen; set => m_sPhanquyen = value; }
+        public string sMatkhau { get => m_sMatkhau; set => m_sMatkhau = value; }
+        public string sEmail { get => m_sEmail; set => m_sEmail = value; }
+    }
+}
