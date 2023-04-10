ebfecd27d399aa714e386136242652958e341ec4
Nguyễn Hoài <hoainhoc101@gmail.com>
2023-04-10 14:17:19 +0700

    comit view list phim

--- EudoraCinema/Controllers/TaikhoanController.cs
+++ EudoraCinema/Controllers/TaikhoanController.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace EudoraCinema.Controllers
@@ -12,18 +12,53 @@ namespace EudoraCinema.Controllers
     public class TaikhoanController : Controller
     {
         // GET: Taikhoan
+        public ActionResult Init()
+        {
+            return View("Login");
+        }
         public ActionResult Login(FormCollection collection)
         {
             using (HttpClient httpClient = new HttpClient())
             {
                 string Email = collection["sEmail"];
                 string Pass = collection["sMatkhau"];
+                try
+                {
+                    using (HttpResponseMessage response = httpClient.GetAsync("https://localhost:44313/api/TaikhoanAPI/" + Email + "/" + Pass).Result)
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                                
+                                return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            // Handle error response
+                            return View();
+                        }
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    return View(); 
+                }
+            }
+        }
+
+        public ActionResult Logout() { return View(); }
+        public ActionResult Index()
+        {
+            List<PhimEntity> lstUser;
+            using (HttpClient httpClient = new HttpClient())
+            {
                 //httpClient.BaseAddress = new Uri(UriString);
-                using (HttpResponseMessage response = httpClient.GetAsync("https://localhost:44313/api/TaikhoanAPI/" + Email+"/"+Pass).Result)
+                using (HttpResponseMessage response = httpClient.GetAsync("https://localhost:44313/api/TaikhoanAPI/" + "GetAll").Result)
                 {
                     if (response.IsSuccessStatusCode)
                     {
-                        return View("Index");
+                        string jsonData = response.Content.ReadAsStringAsync().Result;
+                        lstUser = JsonConvert.DeserializeObject<List<PhimEntity>>(jsonData);
+                        return View(lstUser);
                     }
                     else
                     {
