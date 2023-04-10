346bcf341d5d70608ad301378b1c8e6081123a5e
Hong Ngoc <ngocnguyen71e11@gmail.com>
2023-04-10 22:14:09 +0700

    commit

--- EudoraCinema/Controllers/AdminController.cs
+++ EudoraCinema/Controllers/AdminController.cs
@@ -19,7 +19,9 @@ namespace EudoraCinema.Controllers
         //5 [HttpPost]
         public ActionResult FilmInsert(FormCollection collection)
         {
-            PhimEntity phimEntity = new PhimEntity();
+            try
+            {
+                PhimEntity phimEntity = new PhimEntity();
             using (HttpClient httpClient = new HttpClient())
             {
                 phimEntity.sTenphim = collection["sTenphim"];
@@ -30,13 +32,13 @@ namespace EudoraCinema.Controllers
                 phimEntity.sNgonngu = collection["sNgonngu"];
                 phimEntity.sMota = collection["sMota"];
                 phimEntity.sAnh = collection["sAnh"];
-                try
-                {
+                
                     using (HttpResponseMessage response = httpClient.PostAsJsonAsync(url_base + direct_insertFilm, phimEntity).Result)
                     {
                         if (response.IsSuccessStatusCode)
                         {
-                            return RedirectToAction("Index");
+                            return RedirectToAction("Index", "Taikhoan");
+                            
                         }
                         else
                         {
@@ -44,11 +46,12 @@ namespace EudoraCinema.Controllers
                             return View();
                         }
                     }
-                }
-                catch (Exception ex) { return View(); }
+               
 
 
             }
+            }
+            catch (Exception ex) { return View(); }
         }
         public ActionResult FilmInsertPage(FormCollection collection)
         {
